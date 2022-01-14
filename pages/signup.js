7161b3a3b85cd6665cf4cb76532ef318cc63fa4d
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2022-01-14 15:21:16 +0400

    feat: sign up route

---
+++ pages/signup.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Signup = () => {
+    return (
+        <>
+            <Head>
+                <title>Imposter Syndrome | Signup</title>
+                <meta name="keywords" content="signup" />
+            </Head>
+            <div>ur mum gae</div>
+        </>
+    );
+}
+
+export default Signup;
